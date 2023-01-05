8468e91da722ba1a35812cd6d6680756616b2603
Tushar <tushar.mahawar@gmail.com>
2023-01-05 14:26:21 +0530

    Moving ListAdapter to a new class.

---
+++ app/src/main/java/com/tinytotsnbites/papapoints/ListAdapter.kt
@@ -0,0 +1,110 @@
+package com.tinytotsnbites.papapoints
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.TextView
+import com.tinytotsnbites.papapoints.utilities.LogHelper
+
+// Custom adapter to bind data to the list view
+class ListAdapter(activity: PointsAndTaskActivity, data: List<Item>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private var data: MutableList<Item> = data.toMutableList()
+
+    //Interface to listen for any change of points.
+    interface UpdatePointsList {
+        fun onPointsGiven(taskID: Long, newRating: Int)
+    }
+
+    private var listener: UpdatePointsList? = null
+
+    fun setOnUpdateListListener(listener: UpdatePointsList) {
+        this.listener = listener
+    }
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return data[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    fun updateData(newData: List<Item>) {
+        data = newData as MutableList<Item>
+        notifyDataSetChanged()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View
+        val holder: ViewHolder
+
+        if (convertView == null) {
+            // Inflate the layout and create a new view holder
+            view = inflater.inflate(R.layout.task_rating_row, parent, false)
+            holder = ViewHolder()
+            holder.textView = view.findViewById(R.id.textView)
+            holder.ratingBar = view.findViewById(R.id.ratingBar)
+            holder.addPointsButton = view.findViewById(R.id.add_points_button)
+            holder.removePointsButton = view.findViewById(R.id.remove_points_button)
+            view.tag = holder
+        } else {
+            // Reuse the view holder
+            view = convertView
+            holder = view.tag as ViewHolder
+        }
+
+        // Bind the data to the view
+        val item = getItem(position) as Item
+        holder.textView.text = item.text
+        holder.ratingBar.rating = item.rating.toFloat()
+        holder.ratingBar.setRatingValueNew(item.rating)
+
+        holder.addPointsButton.setOnClickListener {
+            val plusButtonSound = MediaPlayer.create(view.context, R.raw.add_points_sound)
+            val newRating = item.rating + 1
+            LogHelper(this).d("New rating after adding is $newRating")
+            listener?.onPointsGiven(item.taskID, newRating)
+            holder.ratingBar.rating = newRating.toFloat()
+            plusButtonSound.start()
+
+        }
+
+        holder.removePointsButton.setOnClickListener {
+            val minusButtonSound = MediaPlayer.create(view.context, R.raw.minus_points_sound)
+            val newRating = item.rating - 1
+            LogHelper(this).d("New rating after subtracting is $newRating")
+            listener?.onPointsGiven(item.taskID, newRating)
+            holder.ratingBar.rating = newRating.toFloat()
+            minusButtonSound.start()
+
+        }
+
+        holder.ratingBar.setOnRatingBarChangeListener { _, _, fromUser ->
+            if(fromUser) {
+                listener?.onPointsGiven(item.taskID, item.rating+1)
+            }
+        }
+        return view
+    }
+
+    // View holder to optimize the list view
+    private class ViewHolder {
+        lateinit var textView: TextView
+        lateinit var ratingBar: CustomRating
+        lateinit var addPointsButton: Button
+        lateinit var removePointsButton: Button
+    }
+}
+
+// Data class to represent an item in the list
+data class Item(val text: String, val taskID: Long, val rating: Int)
--- app/src/main/java/com/tinytotsnbites/papapoints/PointsAndTaskActivity.kt
+++ app/src/main/java/com/tinytotsnbites/papapoints/PointsAndTaskActivity.kt
@@ -68,7 +68,6 @@ class PointsAndTaskActivity : AppCompatActivity(), ListAdapter.UpdatePointsList 
     }
 
     private fun showChildDetails(child:Child) {
-
         val dateFormat = SimpleDateFormat("dd MMM yy")
         val dateText = findViewById<TextView>(R.id.DayText)
         dateText.text = dateFormat.format(Calendar.getInstance().time)
@@ -76,12 +75,9 @@ class PointsAndTaskActivity : AppCompatActivity(), ListAdapter.UpdatePointsList 
         val nameTextView = findViewById<TextView>(R.id.text_view_name)
         nameTextView.text = child.name
 
-
         val imageView = findViewById<ImageView>(R.id.imageView)
         val gender = child.gender
-
         LogHelper(this).d("Gender is $gender")
-
         if(gender.equals("Male")) {
             imageView.setImageResource(R.drawable.male_image)
         }
@@ -194,101 +190,3 @@ class PointsAndTaskActivity : AppCompatActivity(), ListAdapter.UpdatePointsList 
         }
     }
 }
-
-// Custom adapter to bind data to the list view
-class ListAdapter(activity: PointsAndTaskActivity, data: List<Item>) : BaseAdapter() {
-
-    private val inflater: LayoutInflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-    private var data: MutableList<Item> = data.toMutableList()
-
-    interface UpdatePointsList {
-        fun onPointsGiven(taskID: Long, newRating: Int)
-    }
-
-    private var listener: UpdatePointsList? = null
-
-    fun setOnUpdateListListener(listener: UpdatePointsList) {
-        this.listener = listener
-    }
-
-    override fun getCount(): Int {
-        return data.size
-    }
-
-    override fun getItem(position: Int): Any {
-        return data[position]
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    fun updateData(newData: List<Item>) {
-        data = newData as MutableList<Item>
-        notifyDataSetChanged()
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view: View
-        val holder: ViewHolder
-
-        if (convertView == null) {
-            // Inflate the layout and create a new view holder
-            view = inflater.inflate(R.layout.task_rating_row, parent, false)
-            holder = ViewHolder()
-            holder.textView = view.findViewById(R.id.textView)
-            holder.ratingBar = view.findViewById(R.id.ratingBar)
-            holder.plusButton = view.findViewById(R.id.add_points_button)
-            holder.minusButton = view.findViewById(R.id.remove_points_button)
-            view.tag = holder
-        } else {
-            // Reuse the view holder
-            view = convertView
-            holder = view.tag as ViewHolder
-        }
-
-        // Bind the data to the view
-        val item = getItem(position) as Item
-        holder.textView.text = item.text
-        holder.ratingBar.rating = item.rating.toFloat()
-        holder.ratingBar.setRatingValueNew(item.rating)
-
-        holder.plusButton.setOnClickListener {
-            val plusButtonSound = MediaPlayer.create(view.context, R.raw.add_points_sound)
-            val newRating = item.rating + 1
-            LogHelper(this).d("New rating after adding is $newRating")
-            listener?.onPointsGiven(item.taskID, newRating)
-            holder.ratingBar.rating = newRating.toFloat()
-            plusButtonSound.start()
-
-        }
-
-        holder.minusButton.setOnClickListener {
-            val minusButtonSound = MediaPlayer.create(view.context, R.raw.minus_points_sound)
-            val newRating = item.rating - 1
-            LogHelper(this).d("New rating after subtracting is $newRating")
-            listener?.onPointsGiven(item.taskID, newRating)
-            holder.ratingBar.rating = newRating.toFloat()
-            minusButtonSound.start()
-
-        }
-
-        holder.ratingBar.setOnRatingBarChangeListener { _, _, fromUser ->
-            if(fromUser) {
-                listener?.onPointsGiven(item.taskID, item.rating+1)
-            }
-        }
-        return view
-    }
-
-    // View holder to optimize the list view
-    private class ViewHolder {
-        lateinit var textView: TextView
-        lateinit var ratingBar: CustomRating
-        lateinit var plusButton: Button
-        lateinit var minusButton: Button
-    }
-}
-
-// Data class to represent an item in the list
-data class Item(val text: String, val taskID: Long, val rating: Int)
