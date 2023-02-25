d28de911b20b2e21ca877b0ff7eb340e96e5a552
tusharmahawar <tushar.mahawar@gmail.com>
2023-02-25 22:59:41 +0530

    Updated play store path and version code.

--- app/src/main/java/com/tinytotsnbites/papapoints/utilities/Constants.kt
+++ app/src/main/java/com/tinytotsnbites/papapoints/utilities/Constants.kt
@@ -5,7 +5,7 @@ package com.tinytotsnbites.papapoints.utilities
  */
 const val DATABASE_NAME = "papapoints.db"
 const val TO_EMAIL_ID = "tinytotsnbites@gmail.com"
-const val PLAY_STORE = "com.android.vending"
+const val PLAY_STORE = "com.tinytotsnbites.papapoints"
 const val APP_NAME = "com.google.android.gm"
 const val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id"
 
